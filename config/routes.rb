7a89fb29f746e4b33411e2e897914c48b206db05
lecharles <charleslozano@gmail.com>
2016-01-25 16:26:24 -0500

    Create route,  controller and view for http://localhost:3000/pictures.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "pictures" => "pictures#index"
+  #point this GET request at the INDEX ACTION in the PICTURES CONTROLLER.
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +57,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
